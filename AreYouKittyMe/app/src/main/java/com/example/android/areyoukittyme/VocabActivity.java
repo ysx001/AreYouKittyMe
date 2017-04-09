35df3c624ad7a3939f79ce08b1770a1074efbba5
Sarah Xu <ysx001@bucknell.edu>
2017-04-08 21:05:38 -0400

    Added activity

---
+++ AreYouKittyMe/app/src/main/java/com/example/android/areyoukittyme/VocabActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.areyoukittyme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VocabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vocab);
+    }
+}
