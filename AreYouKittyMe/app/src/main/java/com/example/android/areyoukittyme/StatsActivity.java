aae7cf00b5d99fdf3b4407254410d3b6e104292a
Sarah Xu <ysx001@bucknell.edu>
2017-04-09 15:41:19 -0400

    Added switching tabs for stats

--- AreYouKittyMe/app/src/main/java/com/example/android/areyoukittyme/StatsActivity.java
+++ AreYouKittyMe/app/src/main/java/com/example/android/areyoukittyme/StatsActivity.java
@@ -1,13 +1,16 @@
 package com.example.android.areyoukittyme;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.areyoukittyme.utilities.TabsPagerAdapter;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
-import android.view.View;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.android.areyoukittyme.utilities.NotificationUtils;
 
@@ -17,10 +20,14 @@ public class StatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
-        // set back arrow to home page
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new TabsPagerAdapter(getSupportFragmentManager(), StatsActivity.this));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
-
-
 
 }
