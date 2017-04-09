b40d51fd82c7b1238645ead599a59ec6ab59d659
Jingya Wu <jw057@bucknell.edu>
2017-04-08 22:20:26 -0400

    TimerActivity

--- AreYouKittyMe/app/src/main/java/com/example/android/areyoukittyme/TimerActivity.java
+++ AreYouKittyMe/app/src/main/java/com/example/android/areyoukittyme/TimerActivity.java
@@ -1,9 +1,16 @@
 package com.example.android.areyoukittyme;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TimerActivity extends AppCompatActivity {
+
+    private Button timerStartBtn;
+    private Button timerBackBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
