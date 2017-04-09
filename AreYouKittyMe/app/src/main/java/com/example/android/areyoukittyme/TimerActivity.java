d6de5d7cefb8f4e8365d44b2b4dc3f8dc591aa7a
Jingya Wu <jw057@bucknell.edu>
2017-04-09 18:24:00 -0400

    Update timer GUI

--- AreYouKittyMe/app/src/main/java/com/example/android/areyoukittyme/TimerActivity.java
+++ AreYouKittyMe/app/src/main/java/com/example/android/areyoukittyme/TimerActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class TimerActivity extends AppCompatActivity {
 
     private Button timerStartBtn;
     private Button timerBackBtn;
+    private EditText hourEditText;
+    private Spinner minSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,11 @@ public class TimerActivity extends AppCompatActivity {
 
         this.timerStartBtn = (Button) findViewById(R.id.timerStartBtn);
         this.timerStartBtn = (Button) findViewById(R.id.timerStartBtn);
+
+        this.minSpinner = (Spinner) findViewById(R.id.minSpinner);
+        String[] items = new String[]{"Minutes", "00", "10", "20", "30", "40", "50"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        this.minSpinner.setAdapter(adapter);
 
         // enable back button to main page
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
