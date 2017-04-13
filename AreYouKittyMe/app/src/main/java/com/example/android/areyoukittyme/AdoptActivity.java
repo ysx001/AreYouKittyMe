8a10f540eb9aa91f39fcdd5d6d45a556f28319a2
Jingya Wu <jw057@bucknell.edu>
2017-04-13 19:43:24 -0400

    Added navigation drawer

--- AreYouKittyMe/app/src/main/java/com/example/android/areyoukittyme/AdoptActivity.java
+++ AreYouKittyMe/app/src/main/java/com/example/android/areyoukittyme/AdoptActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.areyoukittyme;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -44,14 +46,29 @@ public class AdoptActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // get the text entered by the user in the EditText
                 String textEntered = catNameTxt.getText().toString();
-                // Store the context variable
-                Context context = AdoptActivity.this;
-                // This is the class we want to start and open when button is clicked
-                Class destActivity = MainActivity.class;
-                // create Intent that will start the activity
-                Intent startMainActivityIntent = new Intent(context, destActivity);
-                startMainActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered);
-                startActivity(startMainActivityIntent);
+
+                if (textEntered.length() > 0) {
+                    // Store the context variable
+                    Context context = AdoptActivity.this;
+                    // This is the class we want to start and open when button is clicked
+                    Class destActivity = MainActivity.class;
+                    // create Intent that will start the activity
+                    Intent startMainActivityIntent = new Intent(context, destActivity);
+                    startMainActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered);
+                    startActivity(startMainActivityIntent);
+                }
+                else {
+                    new AlertDialog.Builder(AdoptActivity.this)
+                            .setTitle(getResources().getString(R.string.title_adopt_name))
+                            .setMessage(getResources().getString(R.string.message_adopt_name))
+                            .setNeutralButton(getResources().getString(R.string.ok_btn), new DialogInterface.OnClickListener() {
+
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            }).show();
+                }
 
             }
         });
