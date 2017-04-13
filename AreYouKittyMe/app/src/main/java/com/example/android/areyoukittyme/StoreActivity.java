207acc99023563debea6d10f474da7caf78f858a
Bicheng Chen <bc029@bucknell.edu>
2017-04-12 22:42:52 -0400

    Now, the drag and drop is working. THIS IS A WORKABLE SOLUTION
    implementing the list of inventory in the store.
    
    commiting before anything goes wrong

--- AreYouKittyMe/app/src/main/java/com/example/android/areyoukittyme/StoreActivity.java
+++ AreYouKittyMe/app/src/main/java/com/example/android/areyoukittyme/StoreActivity.java
@@ -7,6 +7,8 @@ package com.example.android.areyoukittyme;
         import android.content.Intent;
         import android.graphics.drawable.Drawable;
         import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.util.Log;
         import android.view.DragEvent;
         import android.view.MotionEvent;
         import android.view.View;
@@ -15,68 +17,78 @@ package com.example.android.areyoukittyme;
         import android.view.View.OnTouchListener;
         import android.view.ViewGroup;
         import android.widget.Button;
+        import android.widget.ImageView;
         import android.widget.LinearLayout;
         import android.view.View.OnClickListener;
         import android.widget.TextView;
 
         import com.example.android.areyoukittyme.Item.Fish;
         import com.example.android.areyoukittyme.Item.Item;
+        import com.example.android.areyoukittyme.Store.Store;
         import com.example.android.areyoukittyme.User.User;
 
         import org.w3c.dom.Text;
 
         import java.util.ArrayList;
 
-public class StoreActivity extends Activity {
+public class StoreActivity extends AppCompatActivity {
 
     private User user;
+    private Store theStore;
 
     Button backToMainBtn;
     Button incrementCashBtn;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_store);
         findViewById(R.id.myimage1).setOnTouchListener(new MyTouchListener());
         findViewById(R.id.myimage2).setOnTouchListener(new MyTouchListener());
         findViewById(R.id.topleft).setOnDragListener(new MyDragListener());
         findViewById(R.id.topright).setOnDragListener(new MyDragListener());
 
-        this.user = new User(100, new ArrayList<Item>());
-
         incrementCashBtn = (Button) findViewById(R.id.AddCashTempBtn);
-        incrementCashBtn.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        incrementCashBtn.setOnClickListener(new MyClickListener());
+
+
+        //TODO: find a way to store the model
+        this.user = new User(100, new ArrayList<Item>());
+        this.theStore = new Store();
+
+        addItemToStore();
+
+    }
+
+    private void addItemToStore() {
+        LinearLayout storeInventory = (LinearLayout) findViewById(R.id.topleft);
+        ImageView view = new ImageView(this);
+        view.setImageResource(R.drawable.fish);
+        view.setOnTouchListener(new MyTouchListener());
+        LinearLayout.LayoutParams parms = new LinearLayout.LayoutParams(170, 170);
+        view.setLayoutParams(parms);
+        storeInventory.addView(view);
+    }
+
+    private final class MyClickListener implements OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == R.id.AddCashTempBtn) {
                 User.cash += 5;
                 TextView lbl = (TextView) findViewById(R.id.currentCashAmount);
                 lbl.setText(String.valueOf(User.cash));
             }
-        });
-
-        backToMainBtn = (Button) findViewById(R.id.backToMainBtn);
-        backToMainBtn.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Context context = StoreActivity.this;
-                // This is the class we want to start and open when button is clicked
-                Class destActivity = MainActivity.class;
-                // create Intent that will start the activity
-                Intent startMainActivityIntent = new Intent(context, destActivity);
-                startActivity(startMainActivityIntent);
-            }
-        });
-
-
-
+        }
     }
 
     private final class MyTouchListener implements OnTouchListener {
         public boolean onTouch(View view, MotionEvent motionEvent) {
             if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
 //                ClipData data = ClipData.newPlainText("", "");
-                ClipData data = ClipData.newPlainText(String.valueOf(Fish.getPrice()), "80");
+//                Item item = view.getId();
+                ClipData data = ClipData.newPlainText("Item price", "80");
                 DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
                         view);
                 view.startDrag(data, shadowBuilder, view, 0);
@@ -88,8 +100,8 @@ public class StoreActivity extends Activity {
         }
     }
 
-
     class MyDragListener implements OnDragListener {
+        //TODO: resolve the drag to same area crash issue.
         Drawable enterShape = getResources().getDrawable(
                 R.drawable.shape_droptarget);
         Drawable normalShape = getResources().getDrawable(R.drawable.shape);
@@ -109,14 +121,14 @@ public class StoreActivity extends Activity {
                     break;
                 case DragEvent.ACTION_DROP:
                     // Dropped, reassign View to ViewGroup
-                    ClipData.Item item = event.getClipData().getItemAt(0);
-                    String str = item.getText().toString();
-                    TextView lbl = (TextView) findViewById(R.id.currentCashAmount);
-                    int curVal = Integer.parseInt(lbl.getText().toString());
-                    curVal -= Integer.parseInt(str);
-                    User.cash = curVal;
-                    str = String.valueOf(curVal);
-                    lbl.setText(str);
+//                    ClipData.Item item = event.getClipData().getItemAt(0);
+//                    String str = item.getText().toString();
+//                    TextView lbl = (TextView) findViewById(R.id.currentCashAmount);
+//                    int curVal = Integer.parseInt(lbl.getText().toString());
+//                    curVal -= Integer.parseInt(str);
+//                    User.cash = curVal;
+//                    str = String.valueOf(curVal);
+//                    lbl.setText(str);
 
                     View view = (View) event.getLocalState();
                     ViewGroup owner = (ViewGroup) view.getParent();
