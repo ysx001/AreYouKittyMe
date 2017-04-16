2add7fbe9499c5cdf6eb2793296887db14d40834
Sarah Xu <ysx001@bucknell.edu>
2017-04-16 14:34:11 -0400

    Added day stats

---
+++ AreYouKittyMe/app/src/main/java/com/example/android/areyoukittyme/StatsDayActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.areyoukittyme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatsDayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats_day);
+    }
+}
